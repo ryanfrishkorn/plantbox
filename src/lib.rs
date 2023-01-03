dd34d9e7764a33a472c0f99aabe3fe5cd0a05503
Ryan Frishkorn <ryan@bluerift.io>
2023-01-03 15:04:05 -0800

    Add Effects and Requirements

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use rand::Rng;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Board {
     pub matrix: Vec<Vec<BoardSection>>,
+    pub size: usize,
 }
 
 impl Board {
@@ -10,23 +11,27 @@ impl Board {
         const BOARD_SIZE: usize = 256;
         // create an empty row
         let mut matrix: Vec<Vec<BoardSection>> = Vec::new();
-        let mut row: Vec<BoardSection> = Vec::new();
 
-        for _ in 0..BOARD_SIZE {
-            let s = BoardSection {
-                conditions: Conditions {
-                    moisture: 0,
-                    oxygen: 0,
-                },
-            };
-            row.push(s);
-        }
-        for _ in 0..BOARD_SIZE {
-            matrix.push(row.clone());
+        for x in 0..BOARD_SIZE { // x-axis
+            let mut row: Vec<BoardSection> = Vec::new();
+            for y in 0..BOARD_SIZE { // y-axis
+                let s = BoardSection {
+                    conditions: Conditions {
+                        light: 0,
+                        moisture: 0,
+                        oxygen: 0,
+                    },
+                    x: x as u64,
+                    y: y as u64,
+                };
+                row.push(s);
+            }
+            matrix.push(row);
         }
 
         Board {
             matrix,
+            size: BOARD_SIZE,
         }
     }
 }
@@ -34,26 +39,84 @@ impl Board {
 #[derive(Clone, Debug)]
 pub struct BoardSection {
     pub conditions: Conditions,
+    pub x: u64,
+    pub y: u64,
 }
 
 #[derive(Clone, Debug)]
 pub struct Conditions {
+    pub light: u64,
     pub moisture: u64,
     pub oxygen: u64,
+}
+
+#[derive(Clone, Debug)]
+pub enum Effect {
+    Light(i64),
+    Moisture(i64),
+    Oxygen(i64),
+}
+
+impl Effect {
+    pub fn append_global(&self, board: &mut Board) {
+        for row in &mut board.matrix {
+            for section in row {
+                self.append_to_section(section);
+            }
+        }
+    }
+
+    pub fn apply_global(&self, board: &mut Board) {
+        for row in &mut board.matrix {
+            for section in row {
+                self.apply_to_section(section);
+            }
+        }
+    }
+
+    pub fn append_to_section(&self, section: &mut BoardSection) {
+        match self {
+            Effect::Light(v) => {
+                section.conditions.light = match section.conditions.light.checked_add_signed(*v) {
+                    Some(v) => v,
+                    None => 0,
+                }
+            },
+            Effect::Moisture(v) => {
+                section.conditions.moisture = match section.conditions.moisture.checked_add_signed(*v) {
+                    Some(v) => v,
+                    None => 0,
+                }
+            },
+            _ => (),
+        }
+    }
+
+    pub fn apply_to_section(&self, section: &mut BoardSection) {
+        match self {
+            Effect::Light(v) => {
+                section.conditions.light = *v as u64;
+            },
+            Effect::Moisture(v) => {
+                section.conditions.moisture = *v as u64;
+            }
+            _ => (),
+        }
+    }
 }
 
 /// Location
 #[derive(Clone, Debug)]
 pub struct Location {
     pub max: u64,
-    pub x: u64,
-    pub y: u64,
+    pub x: usize,
+    pub y: usize,
 }
 
 impl Location {
     pub fn set_random(&mut self) {
-        self.x = rand::thread_rng().gen_range(0..=self.max);
-        self.y = rand::thread_rng().gen_range(0..=self.max);
+        self.x = rand::thread_rng().gen_range(0..=self.max as usize);
+        self.y = rand::thread_rng().gen_range(0..=self.max as usize);
     }
 
     pub fn new_random() -> Location {
@@ -76,11 +139,18 @@ impl Location {
 #[derive(Clone, Debug)]
 pub struct Plant {
     pub age: u64,
-    pub health: u16,
+    pub health: u64,
     pub kind: PlantKind,
     pub location: Location,
     pub longevity: u64,
     pub messages: Vec<String>,
+    pub requirements: Requirements,
+}
+
+#[derive(Clone, Debug)]
+pub struct Requirements {
+    pub light: Effect,
+    pub moisture: Effect,
 }
 
 impl Plant {
@@ -96,10 +166,10 @@ pub enum PlantKind {
 }
 
 impl Evolve for Plant {
-    fn evolve(&mut self) {
+    fn evolve(&mut self, section: &mut BoardSection) {
         // Save current state for comparison after evolution
         let previous = self.clone();
-        self.biology();
+        self.biology(section);
         if self.health == 0 && previous.health != 0 {
             self.messages.push(format!("The {:?} perishes", self.kind));
         }
@@ -118,19 +188,35 @@ impl Lifespan for Plant {
         false
     }
 
-    fn biology(&mut self) {
+    fn biology(&mut self, section: &mut BoardSection) {
         if self.alive() {
             self.age += 1;
             // death upon exhaustion of lifespan
             if self.age >= self.longevity {
                 self.health = 0;
             }
-            // respiration
-            self.breathe();
+
+            // Respiration
+            match self.requirements.moisture {
+                Effect::Moisture(v) => {
+                    if section.conditions.moisture >= v as u64 {
+                        // consume moisture from section
+                        section.conditions.moisture -= v as u64;
+                    } else {
+                        // take damage
+                        self.damage(1);
+                    }
+                },
+                _ => (),
+            }
         }
     }
 
-    fn breathe(&self) {
+    fn damage(&mut self, damage: u64) {
+        self.health = match self.health.checked_sub(damage) {
+            Some(v) => v,
+            None => 0,
+        }
     }
 }
 
@@ -142,14 +228,11 @@ pub struct Rock {
 }
 
 impl Rock {
-    fn biology(&mut self) {
-        self.age += 1;
-    }
 }
 
 impl Evolve for Rock {
-    fn evolve(&mut self) {
-        self.biology();
+    fn evolve(&mut self, section: &mut BoardSection) {
+        self.age += 1
     }
 
     fn print_age(&self) {
@@ -160,12 +243,12 @@ impl Evolve for Rock {
 /// Father Time wants his incremental payments. All effects that are the result of passing
 /// time should be invoked through this trait.
 pub trait Evolve {
-    fn evolve(&mut self);
+    fn evolve(&mut self, section: &mut BoardSection);
     fn print_age(&self);
 }
 
 pub trait Lifespan {
     fn alive(&self) -> bool;
-    fn biology(&mut self);
-    fn breathe(&self);
+    fn biology(&mut self, section: &mut BoardSection);
+    fn damage(&mut self, damage: u64);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,53 @@ use plantbox::*;
 fn main() {
     let mut tick: u64 = 0;
     let tick_max: u64 = 20;
-    let sleep_duration = time::Duration::from_millis(0);
+    let sleep_duration = time::Duration::from_millis(100);
 
     let mut entities_plants: Vec<Plant> = Vec::new();
     let mut entities_rocks: Vec<Rock> = Vec::new();
 
     // Create a matrix 256x256
-    let board = Board::new();
+    let mut board = Board::new();
 
     // Plant object
     let plant = Plant {
         age: 0,
         health: 10,
         kind: PlantKind::Fern,
-        location: Location::new_random(),
+        // location: Location::new_random(),
+        location: Location {
+            max: 256,
+            x: 7,
+            y: 7,
+        },
         longevity: 12,
         messages: Vec::new(),
+        requirements: Requirements {
+            light: Effect::Light(20),
+            moisture: Effect::Moisture(2),
+        }
     };
     entities_plants.push(plant);
+
+    // Tree object
+    let tree = Plant {
+        age: 0,
+        health: 18,
+        kind: PlantKind::Tree,
+        // location: Location::new_random(),
+        location: Location {
+            max: 256,
+            x: 7,
+            y: 7,
+        },
+        longevity: 230,
+        messages: Vec::new(),
+        requirements: Requirements {
+            light: Effect::Light(20),
+            moisture: Effect::Moisture(4),
+        }
+    };
+    entities_plants.push(tree);
 
     // Rock object
     let rock = Rock {
@@ -55,27 +84,32 @@ fn main() {
                 print!("{} {}\n", indent_dyn(1), m);
             }
             e.messages.clear();
-            print!("{} {}", indent_dyn(1), e.summary());
-            if !e.alive() {
-                print!(" [DEAD]");
+            if e.alive() {
+                print!("{} {}\n", indent_dyn(1), e.summary());
+                // read current conditions for this plant
+                print!("{} {:?}\n", indent_dyn(2), board.matrix[e.location.x as usize][e.location.y as usize]);
             }
-            print!("\n");
-            // read current conditions for this plant
-            print!("{} {:?}\n", indent_dyn(2), board.matrix[e.location.x as usize][e.location.y as usize]);
-
         }
         for e in &entities_rocks {
             print!("{} {:?}\n", indent_dyn(1), e);
         }
 
-        // propagate environmental conditions to proper locations
+        // set all light values to zero before recalculation cycle
+        Effect::Light(0).apply_global(&mut board);
+        // light consistently emitted unless modifiers are present from other sources
+        let sun = Effect::Light(70);
+        sun.apply_global(&mut board);
+
+        // rain is consistent everywhere for now
+        let rain = Effect::Moisture(5);
+        rain.apply_global(&mut board);
 
         // evolve all entities
         for e in &mut entities_rocks {
-            e.evolve();
+            e.evolve(&mut board.matrix[e.location.x][e.location.y]);
         }
         for e in &mut entities_plants {
-            e.evolve();
+            e.evolve(&mut board.matrix[e.location.x][e.location.y]);
         }
 
         // check all living entities for death
